55a9aac17c49d2888323e49d0b33a8d69ecf576d
kush <kushagrasetia@gmail.com>
2019-07-14 08:18:01 +0530

    adding all the files

---
+++ js/post.js
@@ -0,0 +1,17 @@
+//adding functionality to edit button
+
+  
+   
+//ading functionality to like button
+
+//adding functionality to comment button
+function commentFunction()
+{
+    var str = document.getElementById("comment").value;
+
+     document.getElementById('comments').innerHTML=document.getElementById('comments').innerHTML+str;
+}
+
+
+
+
