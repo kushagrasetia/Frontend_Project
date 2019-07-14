33768205241cc4321cce1ab46e01f7a72a92240f
kush <kushagrasetia@gmail.com>
2019-07-14 09:33:11 +0530

    adding functionality to edit button

--- js/post.js
+++ js/post.js
@@ -1,5 +1,30 @@
 //adding functionality to edit button
 
+var editBtn = document.getElementById('editBtn');
+var editables = document.querySelectorAll('#blogText, #blogTitle');
+ 
+editBtn.addEventListener('click', function() {
+  if (!editables[0].isContentEditable) {
+    
+     editBtn.innerHTML = ' Save' + ' <i class="fa fa-save"></i>';
+    editables[0].contentEditable = 'true';
+    editables[1].contentEditable = 'true';
+   
+   
+  } 
+  else {
+    // Disable Editing
+   
+     editBtn.innerHTML = 'Edit ' + '<i class="fa fa-pencil-square-o" aria-hidden="true"></i>';
+    editables[0].contentEditable = 'false';
+    editables[1].contentEditable = 'false';
+    
+    // Save the data in localStorage 
+    for (var i = 0; i < editables.length; i++) {
+      localStorage.setItem(editables[i].getAttribute('id'), editables[i].innerHTML);
+    }
+  }
+});
   
    
 //ading functionality to like button
